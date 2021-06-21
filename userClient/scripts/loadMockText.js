463742b98fb8f8b0558749eadfb9032d555e61a8
alicedumea <79075850+alicedumea@users.noreply.github.com>
2021-06-22 01:57:41 +0300

    added loadMockText (background for users app)

---
+++ userClient/scripts/loadMockText.js
@@ -0,0 +1,49 @@
+const mockText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent libero ligula, dictum ac velit at, imperdiet
+consectetur elit. Pellentesque condimentum erat et tincidunt scelerisque. Quisque vel lobortis dolor. Duis felis
+neque, maximus vel odio nec, dapibus consectetur tellus. Morbi semper nisi justo, a euismod dolor cursus vitae.
+Mauris vel purus luctus, aliquet quam et, egestas leo. Nunc sed velit nisl. Duis rhoncus, lectus non fringilla
+hendrerit, quam ex lacinia orci, nec aliquam leo metus in arcu.
+
+Donec interdum turpis eu diam congue tincidunt. Ut accumsan molestie nisi, vitae ullamcorper nisl gravida non.
+Vestibulum fermentum ex at mauris lacinia pretium ac nec massa. Vestibulum sollicitudin sit amet nulla sed
+suscipit. In rutrum sem nisl, quis tempor nibh luctus ac. Integer interdum risus id sapien finibus vestibulum.
+Nam id est non augue molestie tincidunt eu sit amet nulla.
+
+Maecenas lectus neque, cursus at sodales eu, porttitor a arcu. Nunc nec posuere justo, eu vestibulum justo. Duis
+eleifend nisi a justo tempor viverra. Sed mi ligula, mattis nec augue ullamcorper, tristique tempus lorem.
+Pellentesque in efficitur quam. Mauris a neque imperdiet, faucibus mauris nec, suscipit ante. Quisque et lorem
+ipsum. Morbi eget dui nunc. Vivamus consectetur placerat ultrices.
+
+Sed id ex arcu. In hac habitasse platea dictumst. Nunc lectus mi, faucibus et nisl nec, eleifend lacinia nisi.
+Integer quis velit eget justo viverra gravida a et quam. Ut iaculis fermentum metus, id egestas dui dictum in.
+Cras laoreet lacinia sapien quis faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+posuere cubilia curae; Sed in dolor neque.
+
+Donec venenatis elit ac consequat aliquam. Duis ac lectus sem. Nullam fringilla fringilla nisi ut ornare. Duis
+risus sapien, bibendum sit amet semper ac, varius eget nibh. Donec sit amet consequat purus. Nam ligula turpis,
+auctor id pharetra quis, molestie et libero. Fusce porttitor aliquam metus, non rutrum ipsum dignissim eget.
+Suspendisse felis augue, ornare ut purus nec, laoreet luctus turpis.
+
+Mauris id lorem ipsum. Cras ipsum sapien, dictum in vulputate eu, interdum cursus diam. Praesent dignissim et
+elit eu tempor. Donec lacus urna, posuere eu pulvinar id, semper vel leo. Quisque lectus felis, tempus nec porta
+sed, venenatis quis dolor. Suspendisse quam mauris, vehicula ac dolor non, dignissim accumsan diam. Mauris vel
+erat cursus, vulputate dui euismod, sodales mauris. Quisque lacinia dui a euismod tincidunt. Etiam arcu nulla,
+gravida quis arcu in, condimentum eleifend nulla. Suspendisse vestibulum mauris in tristique vehicula. Morbi
+ornare enim magna, non iaculis nibh faucibus a. Donec varius tristique tortor, nec fringilla dui fringilla nec.
+
+Donec porta, erat ultricies posuere posuere, eros quam molestie est, et tincidunt metus nunc vel felis. Maecenas
+maximus dictum volutpat. Praesent sed libero purus. Quisque bibendum nisl id maximus pretium. Cras tincidunt
+ante eget feugiat vestibulum. Nullam dictum hendrerit dolor, lacinia sagittis velit bibendum sit amet. Praesent
+condimentum nunc vel aliquam tempor.
+
+Phasellus ultricies varius sodales. Morbi vitae turpis accumsan, faucibus odio eu, sagittis mi. Nullam aliquet
+dui eget ex luctus condimentum et a nunc. Maecenas euismod maximus ex, ac fermentum nulla interdum vitae.
+Maecenas ac tempor sapien, nec tempor lorem. Proin vehicula consequat pharetra. Curabitur erat mauris, venenatis
+vel imperdiet at, ornare finibus orci. Nulla blandit orci ut erat venenatis, at vulputate felis porttitor.
+Nullam a finibus turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Curabitur suscipit elementum magna rutrum sollicitudin. Nam at nulla sit amet lectus commodo porta vel eu est.`;
+
+// functie care imi incarca in pagina textul mock-uit
+window.onload = function () {
+	document.getElementById('mock-text').innerHTML = mockText;
+};
