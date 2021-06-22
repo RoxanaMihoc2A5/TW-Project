e6e0be892b8748f9c9e936f61eb492813957b9a4
alicedumea <79075850+alicedumea@users.noreply.github.com>
2021-06-22 03:07:43 +0300

    some minor fixes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const restapiServer = require('./controllers/rest_api');
+const restapiServer = require('./rest_api');
 
 restapiServer.listen(process.env.PORT, () => {
   console.log('Listening on port', process.env.PORT);
